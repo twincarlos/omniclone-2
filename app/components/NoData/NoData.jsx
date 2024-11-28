4f9dcb6eac67f76d0235ae6be5bfe989d6fa9b21
twincarlos <twincarlos98@gmail.com>
2024-11-27 19:17:46 -0500

    added no data style fix

--- app/components/NoData/NoData.jsx
+++ app/components/NoData/NoData.jsx
@@ -2,7 +2,7 @@ import "./NoData.css";
 
 export default function NoData({ icon, message }) {
     return (
-        <div className="no-data">
+        <div className="padding-x2 no-data">
             {icon}
             {message}
         </div>
